aa9de142d80c98452c6ad664ab5f9f8d0d814894
ken <kentuei05@gmail.com>
2025-06-22 14:32:01 +0300

    Use the session object to authenticate a user.

--- server/seed.py
+++ server/seed.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from random import randint
-
 from faker import Faker
 
 from app import app
@@ -15,41 +14,45 @@ with app.app_context():
     Article.query.delete()
     User.query.delete()
 
-    fake = Faker()
-
     print("Creating users...")
     users = []
     usernames = []
     for i in range(25):
-
         username = fake.first_name()
         while username in usernames:
             username = fake.first_name()
-        
-        usernames.append(username)
 
+        usernames.append(username)
         user = User(username=username)
         users.append(user)
 
+    # Add the specific user 'Tuei'
+    tuei_user = User(username="Tuei")
+    users.append(tuei_user)
+
     db.session.add_all(users)
+
+    print("Usernames added:")
+    for user in users:
+        print(f"- {user.username}")
 
     print("Creating articles...")
     articles = []
     for i in range(100):
         content = fake.paragraph(nb_sentences=8)
         preview = content[:25] + '...'
-        
+
         article = Article(
             author=fake.name(),
             title=fake.sentence(),
             content=content,
             preview=preview,
-            minutes_to_read=randint(1,20),
+            minutes_to_read=randint(1, 20),
         )
 
         articles.append(article)
 
     db.session.add_all(articles)
-    
+
     db.session.commit()
-    print("Complete.")
+    print("Seeding complete.")
